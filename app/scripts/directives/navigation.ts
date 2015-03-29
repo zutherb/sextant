cc50ab67f9ddc8fd4819934d95e98b1a602675eb
Bernd Zuther <bernd.zuther@codecentric.de>
2015-03-29 15:10:36 +0200

    add navigation directive

---
+++ app/scripts/directives/navigation.ts
@@ -0,0 +1,57 @@
+/// <reference path="../app.ts"/>
+/// <reference path="../types.ts"/>
+
+
+interface INavigationScope extends ng.IScope {
+    items: any;
+    cssClass(hash :string): string;
+}
+
+sextant.directive("navigation", [():ng.IDirective => {
+    var directive:ng.IDirective = {};
+
+    directive.restrict = "AE";
+    directive.templateUrl = "/partials/navigation.html";
+    directive.replace = true;
+
+    directive.link = (scope: INavigationScope) => {
+        scope.items = [
+            {
+                name: "Home",
+                hash: "#/"
+            },
+            {
+                name: "Minions",
+                hash: "#/minions"
+            },
+            {
+                name: "Pods",
+                hash: "#/pods"
+            },
+            {
+                name: "Replication Controller",
+                hash: "#/rc"
+            },
+            {
+                name: "Services",
+                hash: "#/services"
+            },
+            {
+                name: "Events",
+                hash: "#/events"
+            }
+        ]
+        scope.cssClass = (hash: string): string => {
+            if(location.hash == hash) {
+                return "active";
+            }
+            "";
+        }
+    };
+
+
+
+
+
+    return directive;
+}]);
